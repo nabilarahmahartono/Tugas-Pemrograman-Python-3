8c61c10fae4638f442d70d5467638d3b138b3e8d
nabilarahmahartono <78465098+nabilarahmahartono@users.noreply.github.com>
2021-05-04 07:38:28 +0700

    Add files via upload

---
+++ test fungsi lambda.py
@@ -0,0 +1,32 @@
+Python 3.9.1 (tags/v3.9.1:1e5d33e, Dec  7 2020, 17:08:21) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:\Users\user\AppData\Local\Programs\Python\Python39\program fungsi lambda 1.py
+masukkan nilai = 5
+hasilnya adalah 10 
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program fungsi lambda 2.py
+Mencari luas segi tiga
+alas : 5
+tinggi : 10
+luas :  25.0
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program fungsi lambda 3.py
+FPB dari  106 dan 24 = 2
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program fungsi lambda 4.py
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program fungsi lambda 4.py
+Masukkan bilangan :6
+Faktor dari  6  adalah : 
+1
+2
+3
+6
+>>> 
+= RESTART: C:/Users/user/AppData/Local/Programs/Python/Python39/program fungsi lambda 5.py
+Jumlahnya adalah: 
+160
+rata-ratanya adalah : 
+40.0
+>>> 
